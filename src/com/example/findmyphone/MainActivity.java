569823a5f0b794383d5a10cd8f51a9d473b518de
Alex Hoffman <alex@localhost>
2015-08-20 15:54:59 -0600

    Added UI and fixed git issues

---
+++ src/com/example/findmyphone/MainActivity.java
@@ -0,0 +1,222 @@
+package com.example.findmyphone;
+import android.support.v7.app.ActionBarActivity;
+
+import android.telephony.SmsManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import com.example.findmyphone.SmsBroadcastReceiver;
+import com.example.findmyphone.EditsPageActivity;
+
+public class MainActivity extends ActionBarActivity /*implements OnItemClickListener*/ {
+	//buttons used to save data
+	Button saveNOButton;
+	Button savePhraseButton;
+	Context ctext;
+	//Textview, to count the number of items in TimerQueue
+	//static TextView timerTextView;
+	
+	//edit text fields used to set up numbers and pass phrases
+	EditText Number;
+	EditText PassPhrase;
+//	File file;
+	int NumberofNumbers = 1;
+    private static MainActivity inst;
+    ArrayList<String> PhoneNos;
+    static List<String> QueueLoad;
+    int counter = 1;
+    ArrayList<String> smsMessagesList = new ArrayList<String>();
+    ListView numListView;
+    ArrayList <String> PassPhrases;
+	AlertDialog.Builder alertDefaultSms;
+
+
+    
+  //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+    @SuppressWarnings("rawtypes")
+	ArrayAdapter arrayAdapter;
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
+
+    @SuppressLint("NewApi") @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        
+        setContentView(R.layout.activity_main);
+        PhoneNos = new ArrayList<String>();
+		PassPhrases = new ArrayList<String>(20);
+
+
+      //Used to 
+        //Set up the array adapter
+
+        //set up the listview
+        numListView = (ListView) findViewById(R.id.listView1);
+        arrayAdapter = new ArrayAdapter<String>(this, 
+        		android.R.layout.simple_list_item_1, PhoneNos);
+        numListView.setAdapter(arrayAdapter);
+        
+        numListView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            	//Initialize and set up a sendable addr
+            	String SendableAddr = "";
+            	SendableAddr = PhoneNos.get(position);
+            	//Set up function link to "clickable"
+                ListClickable(SendableAddr,position);   
+                }
+              });
+        //set the button that saves the number to determine what number can send the passphrase to
+        //get the image
+        //saveNOButton = (Button) findViewById(R.id.button1);
+        //set the button that saves the passphrase to determine what is sent to get the image
+        savePhraseButton = (Button) findViewById(R.id.button2);
+        
+        //set up edit text fields
+        //Number = (EditText) findViewById(R.id.editText1);
+        PassPhrase = (EditText) findViewById(R.id.editText2);
+        
+        
+        //load the recently previous passcode used
+        try{
+        	FileInputStream countIn = openFileInput("CounterData");
+        	counter = countIn.read();
+        	countIn.close();
+        	
+        }
+        catch(Exception e){
+        	
+        }
+        try{
+            FileInputStream fin = openFileInput("mydata");
+            
+            int c;
+            String temp="";
+            
+            while( (c = fin.read()) != -1){
+               temp = temp + Character.toString((char)c);
+            }
+        	SmsBroadcastReceiver.setPass(this,temp);
+        	fin.close();
+         }
+         catch(Exception e){
+         }
+        
+        while(NumberofNumbers <= counter)
+        {
+         try{
+        	FileInputStream fin1 = openFileInput("PhoneNumber" + NumberofNumbers);
+        	int c;
+        	String temp="";
+        	
+        	while( (c = fin1.read()) != -1){
+        		temp = temp + Character.toString((char)c);
+        
+        	}
+        	SmsBroadcastReceiver.setPhoneNos(this,temp);
+        	PhoneNos.add(temp);
+        	fin1.close();
+        	
+         }
+         catch(Exception e){
+        	
+         }
+         NumberofNumbers++;
+        }
+        for(int phraseCounter = 0; phraseCounter < counter;phraseCounter++)
+        {
+         try{
+        	 FileInputStream fin2 = openFileInput("PhraseSave" +phraseCounter);
+        	 int c;
+        	 String temp="";
+        	 
+        	 while( (c = fin2.read()) != -1){
+        		 temp = temp+Character.toString((char) c);
+        		 
+        	 }
+        	 //Toast.makeText(this, "counter: " + phraseCounter, Toast.LENGTH_LONG).show();
+        	 
+        	 PassPhrases.add(phraseCounter,temp);
+        	// Toast.makeText(this, PassPhrases.get(phraseCounter) + " Counter: " +phraseCounter, Toast.LENGTH_LONG).show();
+        	 fin2.close();
+            }
+         catch(Exception e)
+         {
+        	
+         }
+        }
+        //Set up the list of contacts that can send the text by accessing internal data
+        //will need to use a for loop to access
+        //Set up the passphrase that is sent to get
+        
+    }
+
+    @SuppressWarnings("unchecked")
+	public void updateList(final String smsAddress) {
+        arrayAdapter.insert(smsAddress, 0);
+        arrayAdapter.notifyDataSetChanged();
+    }
+    
+    public void SAVEPHRASE(View view){
+    	//savephrase should automatically save a passphrase to a global used
+    	//whenever the text is sent
+    	//password = PassPhrase.getText().toString();
+    	//Call upon the function in SmsBroadcastReceiver to store the password
+    	SmsBroadcastReceiver.setPass(this,PassPhrase.getText().toString());
+    	String temp = PassPhrase.getText().toString();
+    	//Save the phrase used to the internal data; whenever created, this data should
+    	//be written back to the phrase. Can be overwritten
+    	try{
+       	 
+       	 FileOutputStream FOS = openFileOutput("mydata",Context.MODE_WORLD_READABLE);
+       	 //should be written to the list again. Should only be an append
+       	 FOS.write((PassPhrase.getText().toString()).getBytes());
+       	 FOS.close();
+       	}
+       	catch (Exception e) {
+               // TO DO Auto-generated catch block
+               e.printStackTrace();
+            }
+    	//Write to edit text, rather than toast it
+    	PassPhrase.setText("");
+    	PassPhrase.setHint("Just set "+temp+" As PassPhrase");
+    }
+    
+   public void EDITNUMBERS(View view){
+	   //editnumbers button should take us to EditsPageActivity, where we can add or delete
+	   //numbers we select from a list
+	   Intent jumpIntent = new Intent(this, EditsPageActivity.class);
+	   startActivity(jumpIntent);
+   }
+   
+    public void ListClickable(String address,int position) {
+    	//used to send a text to the specific number
+    	//Depending on the item clicked, sends a text to that number in question by using the address
+    	SmsManager smsManager = SmsManager.getDefault();
+    	//Toast.makeText(this, "Position: " + position, Toast.LENGTH_LONG).show();
+    	//Toast.makeText(this, PassPhrases.get(position), Toast.LENGTH_LONG).show();
+    	smsManager.sendTextMessage(address, null, PassPhrases.get(position), null, null);
+    }
+}
---
+++ src/com/example/findmyphone/SmsBroadcastReceiver.java
@@ -0,0 +1,192 @@
+package com.example.findmyphone;
+
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.location.Location;
+import android.app.IntentService;
+import android.location.LocationProvider;
+import android.location.LocationManager;
+import android.location.LocationListener;
+import android.app.AlarmManager;
+
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.util.Timer;
+
+
+
+
+
+
+
+
+import android.os.CountDownTimer;
+
+@SuppressLint("NewApi") 
+public class SmsBroadcastReceiver extends BroadcastReceiver {
+	//set up an empty location and an mContext
+	Location location;
+	Location newLocation;
+	private MalibuCountDownTimer CountDownTimer;
+	private final long startTime = 300000;
+	private final long interval = 1000;
+	static String Password;
+	static List<String> PhoneNumber = new ArrayList<String>();
+	static List<String> TimerQueue = new ArrayList<String>();
+	static int TimerQueueSize = 0;
+	//static String PhoneNumber;
+	static String allowedAddress;
+	static String oldAddress;
+    public static final String SMS_BUNDLE = "pdus";
+    String smsBody;
+    static boolean isTimerActive = false;
+        
+    @TargetApi(Build.VERSION_CODES.DONUT) @SuppressLint("NewApi") 
+      public void onReceive(Context context, Intent intent) {
+    	
+    	//Set up the countdown timer for the second text
+		CountDownTimer = new MalibuCountDownTimer(startTime, interval);
+		//make sure the context matches that of onReceive, or else horrible bugs will likely appear
+		CountDownTimer.context = context;
+
+    	
+    	//set up location manager to get the location of the phone
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        //fill location with the last known location of the phone, which should be the current location after being texted
+        location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        
+        Bundle intentExtras = intent.getExtras();
+        if (intentExtras != null) {
+            Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
+            String smsMessageStr = "";
+            for (int i = 0; i < sms.length; ++i) {
+                SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
+
+                smsBody = smsMessage.getMessageBody();
+                String address = smsMessage.getOriginatingAddress();
+                //Get the latitude and longitude to return the google maps image of location
+                if((PhoneNumber.contains(address) != false)&& smsBody.equals(Password))
+                {
+                 SmsManager smsManager = SmsManager.getDefault();
+                 double latitude = location.getLatitude();
+                 double longitude = location.getLongitude();
+                 allowedAddress = address;
+                //send the text out!
+                 smsManager.sendTextMessage(address, null, "I'm at: " + "https://google.com/maps?q="+latitude+","+longitude+"+(My+Point)&z=14&ll="+latitude+","+longitude,null,null);
+                 
+                 //update the queue for the location update
+                 TimerQueue.add("+"+address);
+                 TimerQueueSize++;
+                 //MainActivity.CountTimerQueue();
+                 
+                 //Do the update
+                 //Need to set count down timer so that it goes off once, and then is done
+                 //best acheived by checking for the proper text. else, all texts will trigger this
+               
+                 if(isTimerActive == false)
+                 {
+                	 isTimerActive = true;
+                	 CountDownTimer.start();
+                 }
+                 
+                }
+             
+            }    
+        }
+    }
+    //public method used in main activity to set the pass phrase
+    public static void setPass(Context context, String PassPhrase){
+    	//set the global for password to what passphrase is
+    	Password = PassPhrase;
+    }
+    //public method used in main activity to set one of the phone numbers
+    public static void setPhoneNos(Context context, String PhoneNo){
+    	//Set numbers allowed to whatever is set inside of PhoneNo
+    	//Note: will want several numbers installed, so will have to make an array which is incremented
+    	//Add a number to the list of strings available
+    	PhoneNumber.add("+" + PhoneNo);
+    	//Toast.makeText(context, "added the number " + PhoneNo, Toast.LENGTH_LONG).show();
+    	
+    }
+    public void killTimer(){
+    	//Use to kill the timer once it is down counting down
+    	CountDownTimer.cancel();
+    }
+    
+    //Unfortunately, MalibuCountDownTimer seems necessary, even though it seems
+    //like it can easily be changed
+    public class MalibuCountDownTimer extends CountDownTimer{
+    	//Function called if the location of the person changes, meant to inform
+    	//the person who texted a while back that the location has changed
+    	//set up sms message to original individual who sent the text, informing them
+    	//of movement
+    	Context context;
+    	public MalibuCountDownTimer(long startTime, long interval)
+		{
+			super(startTime, interval);
+		}
+    	
+
+		@Override
+		public void onFinish() {
+			//Set up the location manager for the countdown timer to determine the current
+			//location
+			
+			LocationManager locationManager = (LocationManager) context
+	        		.getSystemService(Context.LOCATION_SERVICE);
+	        //fill location with the last known location of the phone, which should be the current location after being texted
+	        newLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+	        //if we have moved in the last few minutes, send a text to the previous individual
+			if(((location.getLatitude() - 0.01) < newLocation.getLatitude()) &&
+				(newLocation.getLatitude() < (location.getLatitude() + 0.01)) && 
+				(location.getLongitude() - 0.01 < newLocation.getLongitude()) &&
+				(newLocation.getLongitude() < (location.getLongitude() + 0.01)))
+			
+	    	{
+				CountDownTimer.start();
+	    	}
+			else
+			{
+				//send the message indicating the movement
+				//Note, this has to be done for every number that asks, so itemsInQueue should increase over time
+				for(int numberIndex = 0; numberIndex < TimerQueue.size(); numberIndex++)
+				{
+				 //After the 5 minutes, if outside the range, send everybody who asked a text!
+				 //Completely dependent on the receiver
+		    	 SmsManager smsManager = SmsManager.getDefault();
+		    	 smsManager.sendTextMessage(TimerQueue.get(numberIndex), null, "I just moved; I'm now at: https://google.com/maps?q="+newLocation.getLatitude()+","+newLocation.getLongitude()+"+(My+Point)&z=14&ll="+newLocation.getLatitude()+","+newLocation.getLongitude(),null,null);
+				}
+				TimerQueue.clear();
+				//set the timer to deactive so a new timer can start
+				isTimerActive = false;
+				//kill the timer so it doesn't repeat itself
+				killTimer();
+			
+			}
+			
+			
+		}
+
+		@Override
+		public void onTick(long arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+    }
+}
